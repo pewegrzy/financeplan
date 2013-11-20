747e6968ef283207fbd02c5b87bbc1f3c8bafe49
Peter Wegrzynek <pewegrzy@googlemail.com>
2013-11-20 14:40:57 +0100

    config Datei angelegt

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Peter Wegrzynek
+ * Date: 20.11.13
+ * Time: 14:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+    $user = "root";
+    $password = "";
+    $db = "financedb";
+    $path = "localhost";
